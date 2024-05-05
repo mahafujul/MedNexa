fb0591f7bdd788d1a65b8499aea0ff31322a52b0
mahafujul <emonmondal083@gmail.com>
2024-05-05 15:51:45 +0530

    data: Introduce Feedback model for storing user feedback

---
+++ src/models/feedbackModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+
+const feedbackSchema = mongoose.model({
+    patient:{
+        type: String,
+        default: "The patient gave no feedback."
+    },
+    doctor:{
+        type: String,
+        default: "The doctor gave no feedback."
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    doctorInfo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'doctors',
+        required: true,
+    },
+    userInfo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+        required: true,
+    },
+})
+
+export const Feedback = mongoose.models.Feedback || mongoose.model('feedbacks', feedbackSchema)
