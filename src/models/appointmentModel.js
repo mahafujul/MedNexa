d0b7b709659531cd03bb872cff68767c4c272cdc
mahafujul <emonmondal083@gmail.com>
2024-05-05 15:49:36 +0530

    data: Introduce Appointment model for storing appointment information

---
+++ src/models/appointmentModel.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose'
+
+const appointmentSchema = new mongoose.Schema(
+  {
+    doctorInfo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'doctors',
+      required: true,
+    },
+    userInfo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'users',
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    selectedTimeSlot: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "pending",
+    },
+    note: {
+      type: String,
+    },
+    completed: {
+      type: Boolean,
+      default: false
+    },
+    feedback: {
+      type: {type: mongoose.Schema.Types.ObjectId},
+      ref: 'feedbacks'
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Appointment = mongoose.models.Appointment || mongoose.model("appointments", appointmentSchema);
