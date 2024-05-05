252c8943623001862b1a71a2cf31d6caaed9919a
mahafujul <emonmondal083@gmail.com>
2024-05-05 15:50:30 +0530

    data: Introduce Admin model for storing admin user information

---
+++ src/models/adminModel.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const adminSchema = mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    username: { type: String, required: true},
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+})
+
+export const Admin = mongoose.models.Admin || mongoose.model('Admin', adminSchema)
+
+
