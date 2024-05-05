f7aa0e3b4be6125d11edb6d53ff12a666697ce9c
mahafujul <emonmondal083@gmail.com>
2024-05-05 20:46:44 +0530

    feat: Add route handler for appointment rescheduling

---
+++ src/app/api/appointments/reschedule/[appointmentId]/route.ts
@@ -0,0 +1,31 @@
+// Import necessary modules and dependencies
+import { NextRequest, NextResponse } from "next/server"; // Next.js server-side request and response modules
+import { Appointment } from "@/models/appointmentModel"; // Import Appointment model
+
+// Define the PUT endpoint to reschedule an appointment
+export default async function PUT(request: NextRequest) {
+    try {
+        // Retrieve data from request body
+        const reqBody = await request.json();
+        const { date, selectedTimeSlot } = reqBody;
+
+        // Retrieve the appointmentId from the request URL
+        const pathname = request.nextUrl.pathname.split('/');
+        const appointmentId = pathname[pathname.length - 1];
+
+        // Find the appointment by its ID and update its date and selectedTimeSlot
+        const appointmentAfterUpdate = await Appointment.findByIdAndUpdate(appointmentId, { date, selectedTimeSlot });
+
+        // Check if the appointment was found and updated
+        if (!appointmentAfterUpdate) {
+            // Return a JSON response indicating no appointment found with the given appointment Id
+            return NextResponse.json({ message: "No appointment found with the given appointment Id.", success: false }, { status: 400 });
+        }
+
+        // Return success response indicating the appointment has been successfully rescheduled
+        return NextResponse.json({ message: "Appointment successfully rescheduled.", success: true }, { status: 200 });
+    } catch (err: any) {
+        // Return a JSON response for internal server error
+        return NextResponse.json({ message: err.message, success: false }, { status: 500 });
+    }
+}
