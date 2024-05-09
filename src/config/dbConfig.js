e264dac9f3ba2b6a7b4984f63d563c0e4f140e34
mahafujul <emonmondal083@gmail.com>
2024-05-09 22:33:18 +0530

    refactor: Change database name to med-nexa

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export async function connect(){
     try{
-        await mongoose.connect(`${process.env.MONGO_URL}`,{dbName: 'nearbypost'})
+        await mongoose.connect(`${process.env.MONGO_URL}`,{dbName: 'med-nexa'})
         console.log("Connection successfully established with MongoDB.")
     }catch(err){
         console.log("MongoDB connection FAILED: ",err)
