5ab8275d054e912196031e200c6f58799d0a2c1d
mahafujul <emonmondal083@gmail.com>
2024-05-10 02:42:00 +0530

    ui: Add Hero component to frontend

---
+++ src/components/Hero.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+function Hero() {
+  return (
+    <section>
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+            <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+            <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+                <Image
+                alt=""
+                src="/doctors.jpg"
+                width={800}
+                height={800}
+                className="absolute inset-0 h-full w-full object-cover"
+                />
+            </div>
+
+            <div className="lg:py-24">
+                <h2 className="text-3xl font-bold sm:text-4xl">Find & Book <span className='text-primary'>Appointment</span> with your fav  <span className='text-primary'>Doctors</span>.</h2>
+
+                <p className="mt-4 text-gray-600">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
+                eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae eius
+                quidem quam repellat.
+                </p>
+
+                <Button className='mt-8'>Explore Now</Button>
+            </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
