50d76a8d29bfdbe3c95449ce8cc6bfb7883cf859
mahafujul <emonmondal083@gmail.com>
2024-05-10 03:14:43 +0530

    ui: Introduce Doctor Directory Page with speciality filtering

---
+++ src/app/search/[cname]/page.tsx
@@ -0,0 +1,150 @@
+"use client"
+import DoctorList from '@/components/DoctorList';
+// import GlobalApi from '@/app/_utils/GlobalApi'
+import React, { useEffect, useState } from 'react'
+
+function Search({params}: any) {
+
+//   const [doctorList,setDoctorList]=useState([]);
+//   useEffect(()=>{
+//     console.log(params.cname);
+//     getDoctors();
+//   },[])
+
+//   const getDoctors=()=>{
+//     GlobalApi.getDoctorByCategory(params.cname).then(resp=>{
+//       setDoctorList(resp.data.data);
+//     })
+//   }
+const doctorList = [
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Orthopedics"
+    },
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Internal Medicine"
+    },
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Radiology"
+    },
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Orthopedics"
+    },
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Orthopedics"
+    },
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Orthopedics"
+    },
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Orthopedics"
+    },
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Orthopedics"
+    },
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Orthopedics"
+    },
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Orthopedics"
+    },
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Orthopedics"
+    },
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Orthopedics"
+    },
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Orthopedics"
+    },
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Orthopedics"
+    },
+    {
+      id: 1,
+      url: "/beautiful-young-female-doctor-looking-camera-office.jpg",
+      Name: "Mahafujul Haque",
+      Year_of_Experience: "15 Years",
+      Address: "Kalyani",
+      specialization: "Orthopedics"
+    },
+  ]
+  return (
+    <div className='mt-5'>
+        <DoctorList heading={params.cname}
+        doctorList={doctorList}
+        />
+    </div>
+  )
+}
+
+export default Search
---
+++ src/app/search/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import CategoryList from '@/components/CategoryList';
+
+function Layout({ children }: any) {
+  return (
+    <div className='grid grid-cols-4'>
+      <div className='hidden md:block'>
+        {/* Category  */}
+        <CategoryList />
+      </div>
+      <div className='col-span-4 md:col-span-3'>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
