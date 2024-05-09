d11cec69f7db73a6d9f91b748303615e8ba64f08
mahafujul <emonmondal083@gmail.com>
2024-05-10 03:29:05 +0530

    refactor: Improve global application layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/sonner"
+import { CookiesProvider } from 'next-client-cookies/server';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +19,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <CookiesProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="md:px-20">
+            <Header></Header>
+            {children}
+          </div>
+          <Footer/>
+          <Toaster />
+        </body>
+      </html>
+    </CookiesProvider>
   );
 }
