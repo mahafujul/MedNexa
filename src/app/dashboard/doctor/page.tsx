137cefff9952f76b211693682f4e3789c9e5d85a
mahafujul <emonmondal083@gmail.com>
2024-05-19 12:36:47 +0530

    refactor: Remove getServerSession and console logs

--- src/app/dashboard/doctor/page.tsx
+++ src/app/dashboard/doctor/page.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "@/app/api/auth/[...nextauth]/options"
 type Props = {}
 
 async function Doctor({}: Props) {
-  const session = await getServerSession(authOptions)
-  console.log(session)
   return (
     <div className='flex h-screen items-center justify-center'>
         <h1 className='text-primary text-[100px] font-bold'>Doctor</h1>
