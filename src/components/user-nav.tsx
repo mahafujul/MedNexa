1d694318b1e18d5573c8686a0aef855798b83b96
mahafujul <emonmondal083@gmail.com>
2024-05-27 23:36:56 +0530

    feat: add UserNav component with user session management and dropdown menu
    - Implement UserNav component to display user avatar and dropdown menu
    - Integrate next-auth for session management to display username and email
    - Include Avatar, Button, and DropdownMenu components for cohesive UI
    - Add options for Profile, Settings, and Log out with keyboard shortcuts
    - Ensure proper handling of user session and conditional rendering based on session data

---
+++ src/components/user-nav.tsx
@@ -0,0 +1,61 @@
+"use client"
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/custom/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { useSession, signOut } from "next-auth/react"
+import { Session } from "next-auth"
+
+export function UserNav() {
+  const { data: session } = useSession() as { data: Session | null }
+
+  const username = session?.user?.name
+  const email = session?.user?.email
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
+          <Avatar className='h-8 w-8'>
+            <AvatarImage src='/avatars/01.png' alt='@shadcn' />
+            <AvatarFallback>SN</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-56' align='end' forceMount>
+        <DropdownMenuLabel className='font-normal'>
+          <div className='flex flex-col space-y-1'>
+            <p className='text-sm font-medium leading-none'>{username}</p>
+            <p className='text-xs leading-none text-muted-foreground'>
+              {email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Profile
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => { signOut() }}>
+          Log out
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
