2b58c0e1dc017756c48f195fbd5f0a25a53d5ae1
mahafujul <emonmondal083@gmail.com>
2024-05-28 00:08:20 +0530

    feat: Add useCheckActiveNav hook to manage active navigation state
    - Implemented useCheckActiveNav custom hook to determine active navigation state based on current pathname and user role.
    - Utilizes Next.js usePathname and next-auth useSession hooks.
    - The checkActiveNav function verifies if the current navigation item matches the active route.

---
+++ src/hooks/use-check-active-nav.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { usePathname } from 'next/navigation';
+import { useSession } from "next-auth/react"
+
+export default function useCheckActiveNav() {
+    const pathname = usePathname();
+    const { data: session } = useSession()
+    const role = session?.user?.role?.toString().toLowerCase();
+
+    const checkActiveNav = (nav: string) => {
+        const pathArray = pathname.split('/').filter((item: any) => item !== '')
+
+        if (nav === `/dashboard/${role}` && pathArray.length < 3) return true
+        
+        const thirdElementOfTheRoute = nav.split('/')[3]
+        return pathArray.includes(thirdElementOfTheRoute)
+    }
+
+    return { checkActiveNav }
+}
