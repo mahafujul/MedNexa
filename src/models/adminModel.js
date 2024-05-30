f4d4a3179d5c7c8e53520c6a7df96c9feac731c3
mahafujul <emonmondal083@gmail.com>
2024-05-30 17:35:41 +0530

    refactor: Remove separate 'createdAt' & 'updatedAt' fields in favor of Mongoose timestamps

--- src/models/adminModel.js
+++ src/models/adminModel.js
@@ -1,20 +1,23 @@
 import mongoose from 'mongoose'
 
-const adminSchema = mongoose.Schema({
+const adminSchema = mongoose.Schema(
+    {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     username: { type: String, required: true},
     email: { type: String, required: true },
     password: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
     role: {
         type: String,
         default: "ADMIN"
     },
     forgotPasswordToken: String,
     forgotPasswordTokenExpiry: Date,
-})
+    },
+    {
+        timestamps: true
+    }
+)
 
 export const Admin = mongoose.models.Admin || mongoose.model('Admin', adminSchema)
 
