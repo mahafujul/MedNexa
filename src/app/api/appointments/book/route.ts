ebb93dc6c0d40a40cc9a298bb9e0be2fde11da58
mahafujul <emonmondal083@gmail.com>
2024-06-08 18:15:04 +0530

    refactor: Add doctor validation and association in appointment booking
    - Imported Doctor model to validate doctor existence during appointment creation.
    - Added check to ensure the doctor exists before booking an appointment.
    - Updated appointment creation logic to associate the appointment with the doctor.
    - Modified responses to handle cases where the doctor is not found.
    - Enhanced user and doctor models to maintain a list of all appointments.

--- src/app/api/appointments/book/route.ts
+++ src/app/api/appointments/book/route.ts
@@ -1,55 +1,86 @@
 // Import necessary modules and dependencies
 import { NextResponse, NextRequest } from "next/server";
-import { connect } from '@/config/dbConfig';  // Import the database connection function
-import { Appointment } from '@/models/appointmentModel';  // Import the Appointment model
-import { User } from '@/models/userModel';  // Import the User model
-import { authOptions } from "@/app/api/auth/[...nextauth]/options";  // Import NextAuth options
-import { getServerSession } from "next-auth/next";  // Import the getServerSession function from NextAuth
+import { connect } from "@/config/dbConfig"; // Import the database connection function
+import { Appointment } from "@/models/appointmentModel"; // Import the Appointment model
+import { User } from "@/models/userModel"; // Import the User model
+import { Doctor } from "@/models/doctorModel";
+import { authOptions } from "@/app/api/auth/[...nextauth]/options"; // Import NextAuth options
+import { getServerSession } from "next-auth/next"; // Import the getServerSession function from NextAuth
 
 // Connect to the database
 connect();
 
 // Endpoint to book an appointment
 export async function POST(request: NextRequest) {
-    try {
-        // Retrieve the session information
-        const session = await getServerSession(authOptions);
-        const userId = session?.user?.userId;
+  try {
+    // Retrieve the session information
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?.userId;
 
-        // Retrieve data from request body
-        const reqBody = await request.json();
-        const { doctorId, date, selectedTimeSlot, note } = reqBody;
+    // Retrieve data from request body
+    const reqBody = await request.json();
+    const { doctorId, date, selectedTimeSlot, note } = reqBody;
 
-        // Find the user with the userId that we retrieved from the session
-        const user = await User.findOne({ _id: userId });
+    // Find the user with the userId that we retrieved from the session
+    const user = await User.findOne({ _id: userId });
 
-        // Check if the user exists who is trying to create the appointment
-        if (!user) {
-            return NextResponse.json({ message: "No user is found with the given userId", success: false }, { status: 400 });
-        }
-
-        // Create the appointment
-        const appointment = new Appointment({
-            doctorInfo: doctorId,
-            userInfo: userId,
-            date,
-            selectedTimeSlot,
-            note
-        });
-
-        // Save the newly created appointment
-        await appointment.save();
-
-        // Add the newly created appointment to the 'allAppointments' array of the user
-        user.allAppointments.push(appointment._id);
-
-        // Save the user after adding the _id of the appointment
-        await user.save();
-
-        // Respond with a success message
-        return NextResponse.json({ message: `Appointment successfully created for doctor with ID ${doctorId} on ${date}` }, { status: 201 });
-    } catch (err: any) {
-        // Return a JSON response for internal server error
-        return NextResponse.json({ message: err.message, success: false }, { status: 500 });
+    // Check if the user exists who is trying to create the appointment
+    if (!user) {
+      return NextResponse.json(
+        { message: "No user is found with the given userId", success: false },
+        { status: 400 }
+      );
     }
+
+    //Find the doctor with the doctorId that we retrived from the request body
+    const doctor = await Doctor.findOne({ _id: doctorId });
+
+    //Check if the doctor exist to whom th user/patiant is trying to book an appointment
+    if (!doctor) {
+      return NextResponse.json(
+        {
+          message: "No doctor is found with the given doctorId.",
+          success: false,
+        },
+        { status: 400 }
+      );
+    }
+    // Create the appointment
+    const appointment = new Appointment({
+      doctorInfo: doctorId,
+      userInfo: userId,
+      date,
+      selectedTimeSlot,
+      note,
+    });
+
+    // Save the newly created appointment
+    await appointment.save();
+
+    // Add the newly created appointment to the 'allAppointments' array of the user
+    user.allAppointments.push(appointment._id);
+
+    // Save the user after adding the _id of the appointment
+    await user.save();
+
+    // add the newly created appointment to the 'allAppointments' array of the doctor
+    doctor.allAppointments.push(appointment._id);
+
+    // Save the doctor after adding the _id of the appointment
+    await doctor.save();
+
+    // Respond with a success message
+    return NextResponse.json(
+      {
+        message: `Appointment successfully created for doctor with ID ${doctorId} on ${date}`,
+      },
+      { status: 201 }
+    );
+  } catch (err: any) {
+    // Return a JSON response for internal server error
+    return NextResponse.json(
+      { message: err.message, success: false },
+      { status: 500 }
+    );
+  }
 }
