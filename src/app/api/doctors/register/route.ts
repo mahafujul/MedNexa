da7a0b8e51f6ad077480d9a7bbadc22f8d052462
mahafujul <emonmondal083@gmail.com>
2024-06-10 00:02:42 +0530

    refactor: Prefix 'dr-' to doctor usernames and add availableSlots field
    - Added prefix 'dr-' to every doctor's username during account creation
    - Included availableSlots field in the new Doctor instance with an initial value of an empty object
    - Minor adjustments for code consistency and clarity

--- src/app/api/doctors/register/route.ts
+++ src/app/api/doctors/register/route.ts
@@ -1,79 +1,95 @@
 // Import necessary modules
-import bcrypt from 'bcryptjs'; // Module for password hashing
-import { NextResponse, NextRequest } from 'next/server'; // Next.js server-side request and response modules
-import { Doctor } from '@/models/doctorModel'; // Import Doctor model
-import { connect } from '@/config/dbConfig'; // Import database connection function
+import bcrypt from "bcryptjs"; // Module for password hashing
+import { NextResponse, NextRequest } from "next/server"; // Next.js server-side request and response modules
+import { Doctor } from "@/models/doctorModel"; // Import Doctor model
+import { connect } from "@/config/dbConfig"; // Import database connection function
 
 // Connect to the MongoDB database
 connect();
 
 // Endpoint for doctor creation
 export async function POST(request: NextRequest) {
-    try {
-        // Parse request body
-        const reqBody = await request.json();
-        
-        // Destructure request body to extract data
-        const {
-          username,
-          password,
-          firstName,
-          lastName,
-          url,
-          email,
-          about,
-          specialization,
-          phoneNumber,
-          address,
-          city,
-          experience,
-          feePerConsultation,
-          facebook,
-          linkedin,
-          twitter,
-          youtube,
-          degrees,
-        } = reqBody;
-        
-        // Check if a user with the given username already exists
-        const userExists = await Doctor.findOne({ username });
-        if (userExists) {
-            return NextResponse.json({ message: "Doctor already exists with the given username", success: false }, { status: 200 });
-        }
-        
-        // Generate salt and hash the password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        
-        // Create a new Doctor instance with the provided data
-        const newUser = new Doctor({
-          username,
-          password: hashedPassword,
-          firstName,
-          lastName,
-          email,
-          url,
-          about,
-          specialization,
-          phoneNumber,
-          address,
-          city,
-          experience,
-          feePerConsultation,
-          facebook,
-          linkedin,
-          twitter,
-          youtube,
-          degrees,
-        });
-        
-        // Save the new doctor to the database
-        await newUser.save();
-        
-        // Return success response
-        return NextResponse.json({ message: "Doctor created successfully", success: true }, { status: 200 });
-    } catch (err: any) {
-        // Return a JSON response for internal server error with a status code (500)
-        return NextResponse.json({ message: 'Internal Server Error', success: false, err: err.message }, { status: 500 });
+  try {
+    // Parse request body
+    const reqBody = await request.json();
+
+    // Destructure request body to extract data
+    const {
+      username,
+      password,
+      firstName,
+      lastName,
+      url,
+      email,
+      about,
+      specialization,
+      phoneNumber,
+      address,
+      city,
+      experience,
+      feePerConsultation,
+      facebook,
+      linkedin,
+      twitter,
+      youtube,
+      degrees,
+    } = reqBody;
+
+    //Prefix dr- with every username
+    const newUsername = `dr-${username}`;
+
+    // Check if a user with the given username already exists
+    const userExists = await Doctor.findOne({ username });
+    if (userExists) {
+      return NextResponse.json(
+        {
+          message: "Doctor already exists with the given username",
+          success: false,
+        },
+        { status: 200 }
+      );
     }
+
+    // Generate salt and hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create a new Doctor instance with the provided data
+    const newUser = new Doctor({
+      username: newUsername,
+      password: hashedPassword,
+      firstName,
+      lastName,
+      email,
+      url,
+      about,
+      specialization,
+      phoneNumber,
+      address,
+      city,
+      experience,
+      feePerConsultation,
+      availableSlots: {},
+      facebook,
+      linkedin,
+      twitter,
+      youtube,
+      degrees,
+    });
+
+    // Save the new doctor to the database
+    await newUser.save();
+
+    // Return success response
+    return NextResponse.json(
+      { message: "Doctor created successfully", success: true },
+      { status: 200 }
+    );
+  } catch (err: any) {
+    // Return a JSON response for internal server error with a status code (500)
+    return NextResponse.json(
+      { message: "Internal Server Error", success: false, err: err.message },
+      { status: 500 }
+    );
+  }
 }
