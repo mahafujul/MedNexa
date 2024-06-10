f175628c71fa9cf89c590835448d7e9cdca15a5a
mahafujul <emonmondal083@gmail.com>
2024-06-11 03:45:10 +0530

    refactor(api): Enhance GET endpoint for retrieving doctor's appointments with and without the date filter
    - Add functionality to filter appointments based on a specified date passed as a query parameter.
    - Modify GET endpoint to accept NextRequest and parse query parameters.
    - Implement date filtering logic in the aggregation pipeline.
    - Ensure the endpoint returns appropriate responses for both cases (with and without the date filter).
    - Maintain sorting of appointments based on the date in increasing order.
    - Update error handling and success messages accordingly.

--- src/app/api/doctors/appointments/get-all/route.ts
+++ src/app/api/doctors/appointments/get-all/route.ts
@@ -5,13 +5,13 @@ import { Doctor } from "@/models/doctorModel";
 import { Appointment } from "@/models/appointmentModel";
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth/next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // Connect to the database
 connect();
 
-// Endpoint to get all the appointments of a specific doctor
-export async function GET() {
+// Endpoint to get all the appointments of a specific doctor (if a date is passed as a Query Parameter then this endpoint will return appointments based on that date)
+export async function GET(request: NextRequest) {
   try {
     // Retrieve the session information to get the doctor's user ID
     const session = await getServerSession(authOptions);
@@ -25,7 +25,70 @@ export async function GET() {
       );
     }
 
-    // Aggregation pipeline to retrieve doctor's appointments with user details
+    // Parse the date from the query parameters
+    const { searchParams } = new URL(request.url);
+    const date = searchParams.get("date");
+
+    if (!date) {
+      // Aggregation pipeline to retrieve doctor's appointments with user details
+      const appointments = await Doctor.aggregate([
+        { $match: { _id: new mongoose.Types.ObjectId(doctorId) } },
+        { $unwind: "$allAppointments" },
+        {
+          $lookup: {
+            from: "appointments",
+            localField: "allAppointments",
+            foreignField: "_id",
+            as: "appointmentDetails",
+          },
+        },
+        { $unwind: "$appointmentDetails" },
+        {
+          $lookup: {
+            from: "users",
+            localField: "appointmentDetails.userInfo",
+            foreignField: "_id",
+            as: "userDetails",
+          },
+        },
+        { $unwind: "$userDetails" },
+        {
+          $project: {
+            _id: "$appointmentDetails._id",
+            status: "$appointmentDetails.status",
+            completed: "$appointmentDetails.completed",
+            timeSlot: "$appointmentDetails.selectedTimeSlot",
+            userName: {
+              $concat: ["$userDetails.firstName", " ", "$userDetails.lastName"],
+            },
+            date: "$appointmentDetails.date",
+          },
+        },
+        { $sort: { date: 1 } }, // Sort appointments based on date in increasing order
+      ]);
+      // Check if any appointments are retrieved
+      if (!appointments.length) {
+        return NextResponse.json(
+          { message: "No appointments found for the user", success: false },
+          { status: 400 }
+        );
+      }
+
+      // Respond with the retrieved appointments
+      return NextResponse.json(
+        {
+          message: "Appointments successfully retrieved.",
+          appointments: appointments,
+        },
+        { status: 200 }
+      );
+    }
+
+    //Else part
+    // Convert the date string to a Date object
+    const dateObject = new Date(date);
+
+    // Aggregation pipeline to retrieve doctor's appointments with user details for the specified date
     const appointments = await Doctor.aggregate([
       { $match: { _id: new mongoose.Types.ObjectId(doctorId) } },
       { $unwind: "$allAppointments" },
@@ -48,6 +111,14 @@ export async function GET() {
       },
       { $unwind: "$userDetails" },
       {
+        $match: {
+          "appointmentDetails.date": {
+            $gte: new Date(dateObject.setHours(0, 0, 0, 0)),
+            $lt: new Date(dateObject.setHours(23, 59, 59, 999)),
+          },
+        },
+      },
+      {
         $project: {
           _id: "$appointmentDetails._id",
           status: "$appointmentDetails.status",
@@ -65,7 +136,10 @@ export async function GET() {
     // Check if any appointments are retrieved
     if (!appointments.length) {
       return NextResponse.json(
-        { message: "No appointments found for the user", success: false },
+        {
+          message: "No appointments found for the user on the specified date",
+          success: false,
+        },
         { status: 400 }
       );
     }
