83bcf178996df4bad35371561fd79a63c490a409
mahafujul <emonmondal083@gmail.com>
2024-06-13 01:23:05 +0530

    feat: Define specializationCategoryList array in a centralized location

---
+++ src/data/specializationCategoryList.tsx
@@ -0,0 +1,87 @@
+export const specializationCategoryList = [
+  {
+    label: "Dermatologist",
+    value: "dermatologist",
+    url: "/skin.png",
+  },
+  {
+    label: "Allergist",
+    value: "allergist",
+    url: "/allergy.png",
+  },
+  {
+    label: "Gastroenterologist",
+    value: "gastroenterologist",
+    url: "/Gastroenterologist.png",
+  },
+  {
+    label: "Neurologist",
+    value: "neurologist",
+    url: "/neurologist.png",
+  },
+  {
+    label: "Cardiologist",
+    value: "cardiologist",
+    url: "/cardiologist.png",
+  },
+  {
+    label: "Gynecologist",
+    value: "gynecologist",
+    url: "/gynecologist.png",
+  },
+  {
+    label: "Pediatrician",
+    value: "pediatrician",
+    url: "/pediatrician.png",
+  },
+  {
+    label: "Hepatologist",
+    value: "hepatologist",
+    url: "/hepatologist.png",
+  },
+  {
+    label: "Osteopathic",
+    value: "osteopathic",
+    url: "/osteopathic_.png",
+  },
+  {
+    label: "Endocrinologist",
+    value: "endocrinologist",
+    url: "/endocrinologist.png",
+  },
+  {
+    label: "Pulmonologist",
+    value: "pulmonologist",
+    url: "/pulmonologist.png",
+  },
+  {
+    label: "Internal Medicine",
+    value: "internal_medicine",
+    url: "/internal_medicine.png",
+  },
+  {
+    label: "Common Cold",
+    value: "common_cold",
+    url: "/common_cold.png",
+  },
+  {
+    label: "Phlebologist",
+    value: "phlebologist",
+    url: "/phlebologist.png",
+  },
+  {
+    label: "Osteoarthritis",
+    value: "osteoarthritis",
+    url: "/osteoarthritis.png",
+  },
+  {
+    label: "Rheumatologists",
+    value: "rheumatologists",
+    url: "/rheumatologists.png",
+  },
+  {
+    label: "Otolaryngologist",
+    value: "otolaryngologist",
+    url: "/otolaryngologist.png",
+  },
+];
