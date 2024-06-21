36c7be5a92663b3452e72229c7323e6d2a312294
mahafujul <emonmondal083@gmail.com>
2024-06-22 01:06:06 +0530

    Create endpoint to retrieve details of a specific doctor
    - Implemented GET endpoint in Next.js to fetch details of a doctor based on the provided doctorId query parameter.
    - Utilized Mongoose aggregate pipeline to query specific fields of the doctor document.
    - Returns a JSON response with doctor details upon successful retrieval.
    - Handles cases where no doctor is found with a 404 status code and provides appropriate error handling for internal server errors (500).

---
+++ src/app/api/admin/doctors/profile/route.ts
@@ -0,0 +1,68 @@
+import { NextResponse, NextRequest } from "next/server";
+import { connect } from "@/config/dbConfig";
+import { Doctor } from "@/models/doctorModel";
+import mongoose from "mongoose";
+
+// Connect to the database
+connect();
+
+// Endpoint to retrieve details of a specific doctor.
+export async function GET(request: NextRequest) {
+  try {
+    const ObjectId = mongoose.Types.ObjectId;
+    const reqUrl = request.url;
+    const { searchParams } = new URL(reqUrl);
+    const doctorId = searchParams.get("doctorId")?.toString();
+
+    // Aggregate query to fetch specific fields of the doctor
+    const doctor = await Doctor.aggregate([
+      {
+        $match: {
+          _id: new ObjectId(doctorId),
+        },
+      },
+      {
+        $project: {
+          // Specify fields to include in the response
+          username: 1,
+          firstName: 1,
+          lastName: 1,
+          url: 1,
+          email: 1,
+          phoneNumber: 1,
+          about: 1,
+          specialization: 1,
+          degrees: 1,
+          address: 1,
+          city: 1,
+          experience: 1,
+          feePerConsultation: 1,
+        },
+      },
+    ]);
+
+    // Handle case where no doctor is found with the given ID
+    if (!doctor.length) {
+      return NextResponse.json(
+        { message: "Doctor not found", success: false },
+        { status: 404 } // Not Found status code
+      );
+    }
+
+    // Return successful response with doctor details
+    return NextResponse.json(
+      {
+        message: "Doctor details retrieved successfully",
+        doctor,
+        success: true,
+      },
+      { status: 200 } // OK status code
+    );
+  } catch (err: any) {
+    // Return a JSON response for internal server error with a status code (500)
+    return NextResponse.json(
+      { message: "Internal Server Error", success: false, error: err.message },
+      { status: 500 } // Internal Server Error status code
+    );
+  }
+}
