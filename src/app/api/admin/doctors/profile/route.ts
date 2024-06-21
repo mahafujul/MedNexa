53b9ee45816da4726911acd0733ffaff3f68213d
mahafujul <emonmondal083@gmail.com>
2024-06-22 01:08:29 +0530

    feat: Implement endpoint to update details of a specific doctor
    - Implemented PUT endpoint in Next.js to update details of a doctor based on the provided doctorId query parameter.
    - Uses Mongoose findOneAndUpdate to find and update the doctor's details with data from the request body.
    - Returns a JSON response with a success message upon successful update.
    - Handles internal server errors with appropriate error message and status code (500).

--- src/app/api/admin/doctors/profile/route.ts
+++ src/app/api/admin/doctors/profile/route.ts
@@ -66,3 +66,35 @@ export async function GET(request: NextRequest) {
     );
   }
 }
+
+// Endpoint to update details of a specific doctor
+export async function PUT(request: NextRequest) {
+  try {
+    // Retrieve data from request body
+    const reqBody = await request.json();
+
+    // Retrieve doctorId from query param
+    const reqUrl = request.url;
+    const { searchParams } = new URL(reqUrl);
+    const doctorId = searchParams.get("doctorId")?.toString();
+
+    // Find and update the doctor's details
+    const updatedDoctor = await Doctor.findOneAndUpdate(
+      { _id: doctorId },
+      { ...reqBody },
+      { new: true } // Return updated document
+    );
+
+    // Return successful response with a message
+    return NextResponse.json(
+      { message: "Doctor profile updated successfully", success: true },
+      { status: 200 } // OK status code
+    );
+  } catch (err: any) {
+    // Return a JSON response for internal server error with a status code (500)
+    return NextResponse.json(
+      { message: "Internal Server Error", success: false, error: err.message },
+      { status: 500 } // Internal Server Error status code
+    );
+  }
+}
